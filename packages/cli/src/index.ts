d2632f2f9fe47c31eabefe6cdc9edf0f37e0a2d7
Ozan Turksever <ozan.turksever@gmail.com>
2026-01-02 14:31:21 +0100

    chore(cli): release v2.0.0

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -3,7 +3,7 @@
 import { Relay } from "./relay.js";
 import { logger, setLogLevel } from "./logger.js";
 
-const VERSION = "1.0.4";
+const VERSION = "2.0.0";
 
 function printUsage(): void {
   console.log(`
